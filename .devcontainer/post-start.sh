77702aeeaeca08ae53915c219f7af58bd0164458
Nicholas Morey <nicholas@morey.tech>
2024-03-06 20:18:53 +0000

    fix(devct): kind cluster name

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,7 +4,7 @@ echo "post-start start" >> ~/status
 
 # this runs in background each time the container starts
 
-# kind delete cluster --name akuity-declarative-example
+# kind delete cluster --name rendered-manifests-example
 # kind create cluster --config .devcontainer/kind-cluster.yaml
 
 echo "post-start complete" >> ~/status
